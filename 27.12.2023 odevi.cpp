29e91d827280cab53a777cac940c43a87cfc7112
Mademoiselle Rose <153909942+mademoiselle-rose@users.noreply.github.com>
2023-12-26 23:30:20 +0300

    Update 27.12.2023 odevi.cpp

--- 27.12.2023 odevi.cpp
+++ 27.12.2023 odevi.cpp
@@ -1,46 +1,61 @@
-void hesaplama(){
-		int a,b;
-		int top=1;
-		int cik=2;
-		int carp=3;
-		int bol=4;
+void hesaplama(int a, int b){
+		float a;
+		float b;
+	
 		
 	printf("ilk sayiyi giriniz:");
-	scanf("%d", a);
+	scanf("%f", &a);
 	
 	printf("ikinci sayiyi giriniz:");
-	scanf("%d", b);
-	
+	scanf("%f", &b);
+		}
+	int main(){
+		
+		int top = a+b;
+		int cik = a-b;
+		int carp = a*b;
+		int bol = a/b;
+		int toplama= 1;
+		int cikarma= 2;
+		int carpma=3;
+		int bolme=4;
+		printf("toplama icin 1 e basiniz, cikarma icin 2 e basiniz, carpma icin 3 e basiniz, bolme icin 4 e basiniz:");
+		
 	  char secim;
 	  printf("islemi giriniz:");
 	  scanf("%c", &secim);
 	  
 	  switch(secim){
-	  	case 1:
-	  	printf("%d", top);
+	  	case 1
+	  	int toplama=(int a + int b);
+	  	printf("%f", toplama);
 	  	break;
 	  	
 	  	case 2:
-	  		printf("%d", cik);
-	  	    break;
+	  	int cikarma=(int a - int b);
+	  	printf("%f", cikarma);
+	  	break;
 	  	    
 	  	case 3:
-		  printf("%d", carp);
-		  break;
+	  	int carpma=(int a * int b);
+		printf("%f", carpma);
+		break;
 		  
 		case 4:
-		printf("%d", bol);
+		int bolme=(int a/ int b);
+		printf("%f", bolme);
 	    break;      
 	    
-	    default:
-	    	printf("hatali islem yaptiniz:");
+	    default:{
+			break;
+			printf("hatali islem yaptiniz:");
+			
 	  }
 	  
 	  
 	  
-		
+		return 0;
+	  	
 	}
-	int main(){
-	  	hesaplama(78,56);
-	  	return 0;
-	  }
+}
+	  
